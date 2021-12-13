0f48769df2bc54b53739885053006f03550624db
krstephens01 <90530886+krstephens01@users.noreply.github.com>
2021-12-12 20:40:52 -0500

    Update dessertdb.js

--- final/dessertdb.js
+++ final/dessertdb.js
@@ -36,10 +36,10 @@ if (req.url != '/favicon.ico') {
 
 	/////////////////////INSERT
 	var newData = {"name": dName, "servings": dServings, "image": dImg, "link": dLink, "time": dTime};
-	coll.insertOne(newData, function(err, res) {
+	//coll.insertOne(newData, function(err, res) {
     	if (err) throw err;
-    	console.log("new document inserted");
-	}); 
+    	//console.log("new document inserted");
+	//}); 
 
 	////////////////////////////QUERY   	
 	theQuery = {};
@@ -49,10 +49,37 @@ if (req.url != '/favicon.ico') {
     	} 
 	else 
     	{
-    	for (i=0; i<items.length; i++){
-	//HTML THAT DISPLAYS IS BELOW (STYLING NEEDS TO BE FIXED)
-	res.write('<img src='+items[i].image+' style= "max-width: 200px;"></br><a href='+items[i].link+'>' + items[i].name + '</a></br><p2>Make Time: ' + items[i].time + 
-		'</p2></br><p3>Servings: ' + items[i].servings + '</p3></br></br>');
+	res.write('<style> body { margin: 0 auto; } #hero { background-image: url("https://raw.githubusercontent.com/alexandra-scott/webmastas/main/final/background.jpg"); background-size: cover; width: 100%; height: 900px; margin-top: -400px; }'+
+	'img { width: 125px; height: 125px; border-radius: 50%; margin-top:12px; margin-left:10px; margin-right: 0;}'+
+	'#top10 { margin-right: 18%; margin-left: 18%; border-radius: 31px; background-color: #FFFFFF; height: 150px; filter: drop-shadow(0px 14px 15px #1c604e); margin-bottom: 1.5%; position: relative; top: -440px; margin-top: 10px;}'+
+	'#wave { background-image: url("https://raw.githubusercontent.com/alexandra-scott/webmastas/main/final/whitewave.png"); margin-top: -139px; background-size:cover; z-index: 10000; height: 150px; width: 100%; background-position: center; }'+	
+	'#trend-head { text-align: left; margin-left: 18%; top: 365px; color: #1c604e; filter: drop-shadow(2px 4px 6px #FFFFFF); }' + 
+	'h1 { font-family: "Varela Round", sans-serif; text-align: center; font-size: 50pt; color: #FFFFFF; filter: drop-shadow(2px 4px 6px #1c604e); position: relative; top: 445px; margin-bottom: 20px; }'+	
+	'#nav a:hover { cursor: pointer; } #nav { text-decoration: none; display: flex; justify-content: flex-start; margin-left: 4%; margin-right: 4%; font-size: 13pt; align-items: center; font-family: "Varela Round", sans-serif; text-align: left; margin-top: 25px; font-size: 16pt; color: #1c604e;}'+
+	'header a:active, a:hover, a:visited, a:link { color: #1c604e; text-decoration: none; }'+
+	'#namer {font-family: "Varela Round", sans-serif;font-size: 250%; color: #FFFFFF; margin-left:12%; position: relative;}'+
+	'.column {float: left; padding-right: 0px;}'+
+	'p2, p3 {font-family: "Varela Round", sans-serif; margin-left: 12%; font-size: 16pt; padding-right: 10px;}'+
+	
+
+	'</style>' + 
+	
+	'<header> <ul id="nav"> <a style="margin-right:4%" href="https://alexandra-scott.github.io/webmastas/final/final-home.html">Go Back</a></header>'+
+	'<div id="hero" style="margin-top: -300px"><h1 id="trend-head">User-Made Recipes</h1></div>' + 
+	'<section id="wave"></section>');
+
+    	
+
+////////////////////////////////////////////////////////LOOP//////////////////////////
+	for (i=0; i<items.length; i++){
+	if (items[i].image == ""){items[i].image = "https://www.nicepng.com/png/full/54-547519_covered-clipart-plate-food-plate-clip-art.png"};
+
+	res.write('<div id = "top10"><div class = "column"><img src='+items[i].image+'></div><div class = column><div id = "namer"><a href='+items[i].link+'>' + 
+	items[i].name + '</div></a></br><p2></div><div class = column>Make Time: ' + items[i].time + 
+	'</p2></br><p3>Servings: ' + items[i].servings + '</p3></br></br></div></div>');
+	
+
+
 	db.close();
 	}
 	res.end();
@@ -62,4 +89,5 @@ if (req.url != '/favicon.ico') {
 });
 }
 }).listen(port);
+
 
